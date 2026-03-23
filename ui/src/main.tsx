9c02749e993c7a98ad7c289b2f7f4ccea101b2d7
Eliahu Garcia Lozano <eliahugarcialozano@gmail.com>
2026-03-23 08:18:21 +0200

    feat: add chatbot ui component with component dependencies

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { TooltipProvider } from './components/ui/tooltip.tsx'
+import ChatbotDemo from './components/ai/chatbot.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<TooltipProvider>
+			<ChatbotDemo />
+			{/* <App /> */}
+		</TooltipProvider>
+	</StrictMode>,
 )
